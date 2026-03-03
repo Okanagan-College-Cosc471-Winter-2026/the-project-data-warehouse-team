3e8b0b5f56f0ec190cb72f860293c5c140659e2b
cosc-admin <cosc-admin@cosc471-ora-group1.localdomain>
2026-03-03 15:38:56 -0800

    Incorporated real historical data into the pipeline. Forgot to make a new branch before starting the work

--- dw-ingestion-api/src/main.py
+++ dw-ingestion-api/src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from src.routers.ingestion import router
 from src.config import get_settings
+from src.routers.ingestion import router as ingestion_router
+from src.routers.historical_ingestion import router as historical_router
 
 settings = get_settings()
 
@@ -10,4 +11,5 @@ app = FastAPI(
     version="0.1.0",
 )
 
-app.include_router(router)
+app.include_router(ingestion_router)
+app.include_router(historical_router)
